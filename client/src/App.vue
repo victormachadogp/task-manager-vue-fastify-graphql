a80cc365e1e28c4ec79cc94fb02cba698f1ec672
Victor Machado <victormachadogp@gmail.com>
2024-11-13 22:40:04 -0300

    Removing header

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,9 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
 <div>
-  <header>
-    <div class="wrapper">
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
   </div>
 </template>
